1a69e71ad142160a23de04914103b26bc3012db7
Vosmash <glennvockings@gmail.com>
2018-12-30 22:43:39 +0000

    Updates

---
+++ precache-manifest.c6af07eb9df0f3991ef64ede0ea95b4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7046b8b219bc4cc09de5",
+    "url": "/static/css/main.a1dbd139.chunk.css"
+  },
+  {
+    "revision": "7046b8b219bc4cc09de5",
+    "url": "/static/js/main.7046b8b2.chunk.js"
+  },
+  {
+    "revision": "77958300264c6869a197",
+    "url": "/static/js/1.77958300.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/static/media/background_photo_desktop.37be7357.jpg"
+  },
+  {
+    "revision": "31186ed596653737e0f80df59e190e95",
+    "url": "/index.html"
+  }
+];
